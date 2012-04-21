cf8e5a59d1f06adb2753daf87d794c9446b3454d
Phil <austermann@klimagipfel.de>
2012-04-21 06:54:55 +0200

    # Refactoring Folder Structure

--- www/index.php
+++ www/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/protected/lib/yii/yii.php';
+$yii=dirname(__FILE__).'../common/lib/yii/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
