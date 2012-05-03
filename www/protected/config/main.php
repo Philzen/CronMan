a2d6c2d1ab7b24908fdb3ed2a06519482bf3cac7
Phil. Austermann <phil@patient-radio.de>
2012-05-03 07:28:01 +0200

    # Corrected Project Name

--- www/protected/config/main.php
+++ www/protected/config/main.php
@@ -6,7 +6,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'CronMan',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
